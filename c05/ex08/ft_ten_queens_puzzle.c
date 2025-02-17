78ae7db05826f2548c7705de2bc2d91a0ec42bfc
dvavryn <vavryn.dominic@outlook.com>
2025-02-17 22:32:12 +0100

    a

--- c05/ex08/ft_ten_queens_puzzle.c
+++
@@ -1,9 +0,0 @@
-int main (void)
-{
-
-}
-
-void ft_ten_queens_puzzle(void)
-{
-	//////aaaaaa////aaaaa////aaa
-}
--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -10,51 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/* 
-	Create a function ft_range which returns an array of ints. This
-	int array should contain all values between min and max.
-
-	Min included - max excluded.
-
-	Here is how it should be prototyped:
-		int 	*ft_range(int min, int max);
-
-	If min's value is greater or equal to max's value, a null pointer
-	should be returned.
- */
 #include <stdlib.h>
-#include <stdio.h>
 
+void	ft_write_array(int min, int max, int *arr);
+
+/* #include <stdio.h>
 int	*ft_range(int min, int max);
-void ft_write_array(int min, int max, int *arr);
 
 int main(void)
 {
-	int *i = ft_range(1, 20);
-	int j = 0;
-	while (*i)
-	{
-		printf("%i ", i[j]);
-		i++;
-		j++;
-	}	
-}
 
+	int min = 1;
+	int max = 20;
+	int *arr = ft_range(min, max);
+
+	if (!arr)
+	{
+		return 1;
+	}
+	int j = 0;
+	while (j < (max - min))
+	{
+		printf("%i ", arr[j]);
+		j++;
+	}
+}
+*/
 int	*ft_range(int min, int max)
 {
+	int	*buf;
+
 	if (min >= max)
 		return (NULL);
-	
-	int *buf;
-
 	buf = malloc((max - min) * sizeof(int));
 	ft_write_array(min, max, buf);
-	return (buf);	
+	return (buf);
 }
 
-void ft_write_array(int min, int max, int *arr)
+void	ft_write_array(int min, int max, int *arr)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < (max - min))
