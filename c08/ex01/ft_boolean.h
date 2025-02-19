03cc6fbbac3768836947c9c8d5910dd88719e024
dvavryn <vavryn.dominic@outlook.com>
2025-02-19 22:28:00 +0100

    a

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+typedef int	t_bool;
+
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 0
+#define EVEN(n) ((n)% 2 == 0)
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
---
+++ c08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,8 @@
+#include "ft_stock_str.h"
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av);
+
+int	main(void)
+{
+	
+}
