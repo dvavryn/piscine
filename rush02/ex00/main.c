9cad4c4aabd8ed325c5aeed9163cf63ae9b6b70a
Dominic Vavryn <dvavryn@c2r3p5.42vienna.com>
2025-02-22 11:25:36 +0100

    a

--- rush02/ex00/main.c
+++ rush02/ex00/main.c
@@ -1,24 +1,19 @@
-int main(int argc, char **argv)
+/*	Prototypes	*/
+void	ft_aaa(char *num, char *dict);
+
+int	main(int argc, char **argv)
 {
 	if (argc == 1)
-	{
-		ft_putstr("Custom dictionary!");
-		ft_putstr("Usage: ./filename \"*.dict\" \"value\"");
-		ft_putstr("Standard dictionary!");
-		ft_putsrt("       ./filename \"value\"");
-	}
+		return (-1);
 	if (argc == 2)
-	{
-
-	}	
+		ft_aaa(argv[1], "numbers.dict");	// if only 1 arg
+		// argv[1] = value to convert
 	if (argc == 3)
-	{
-		
-	}
+		ft_aaa(argv[2], argv [1]);			// if 2 arg 
+		// argv[2] = dict name
+		// argv[1] = value to convert
 }
 
-void	ft_putstr(char *str)
+void	ft_aaa(char *num, char *dict)
 {
-	while (*str)
-		write(1, str++, 1);
 }
---
+++ rush02/ex00/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	char *filename;
+	int readline = 0;
+	int opened = 0;
+	filename = "numbers.dict";
+
+	opened = open
+	printf("Filename: %s", filename);
+
+	return 0;
+}
