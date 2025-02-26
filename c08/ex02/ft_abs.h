5fcbb5ec711215ae814d9ad33c21adf0822301ca
Dominic Vavryn <dvavryn@c3r5p5.42vienna.com>
2025-02-26 13:39:02 +0100

    a

--- c08/ex02/ft_abs.h
+++ c08/ex02/ft_abs.h
@@ -6,20 +6,19 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:12:35 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/02/25 12:14:44 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/02/26 13:34:33 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
+# define ABS(val) abs(val)
 
-//# define ABS(val) (val < 0 ? -val : val)
-# define ABS(val) ({ \
-    typeof(val) _val = (val); \
-    if (_val < 0) { \
-        -_val; \
-    } else { \
-        _val; \
-    } \
-})
+int	abs(int val)
+{
+	if (val < 0)
+		return (-val);
+	return (val);
+}
+
 #endif
