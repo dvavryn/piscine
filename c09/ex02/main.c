27fe21a52cb61cae37f721865dbcc28d75dd8218
Dominic Vavryn <dvavryn@c3r5p5.42vienna.com>
2025-02-27 16:17:23 +0100

    a

--- c09/ex02/main.c
+++ c09/ex02/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	issep(char c, char *charset);
+int count_words(char *string, char *charset);
+char **ft_split(char *string, char *charset);
+
 int main (void)
 {
 	char *string = "Hello i am a test string";
@@ -12,11 +16,48 @@ int main (void)
 	while (hello[i] != NULL)
 	{
 		printf("%s", hello[i]);
+		i++;
 	}
-	return ;
 }
 
 char **ft_split(char *string, char *charset)
 {
+	char	**ret;
+	int		word_count;
 
+	word_count = count_words(string, charset);
+	ret = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (!ret)
+	{
+		free(ret);
+		return (ret);
+	}
+	return (ret);
+}
+
+int count_words(char *string, char *charset)
+{
+	int	words;
+	int i;
+
+	words = 0;
+	i = 0;
+	while (string[i] != '\0')
+	{
+		if (issep(string[i], charset) == 1 && issep(string[i+1], charset) == 0)
+			words++;
+		i++;
+	}
+	return (words - 1);
+}
+
+int	issep(char c, char *charset)
+{
+	int i;
+
+	i = 0;
+	while (charset[i] != '\0')
+		if (c == charset[i++])
+			return (1);
+	return 0;
 }
